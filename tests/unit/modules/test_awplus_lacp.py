f3e676b830dbd14fdda5a853f852b220d9ddbbf4
dalyIsaac <isaac.daly@outlook.com>
2020-02-17 16:58:44 +1300

    Added lacp playbook and fixed imports for test_awplus_lacp.py.

--- tests/unit/modules/test_awplus_lacp.py
+++ tests/unit/modules/test_awplus_lacp.py
@@ -2,12 +2,13 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 #
 
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 
-from units.compat.mock import patch
-from ansible.modules.network.awplus import awplus_lacp
-from units.modules.utils import set_module_args
+from ansible_collections.alliedtelesis.awplus.tests.unit.compat.mock import patch
+from ansible_collections.alliedtelesis.awplus.plugins.modules import awplus_lacp
+from ansible_collections.alliedtelesis.awplus.tests.unit.utils import set_module_args
 from .awplus_module import TestAwplusModule, load_fixture
 
 
@@ -18,27 +19,37 @@ class TestAwplusLacpInterfacesModule(TestAwplusModule):
         super(TestAwplusLacpInterfacesModule, self).setUp()
 
         self.mock_get_config = patch(
-            'ansible.module_utils.network.common.network.Config.get_config')
+            "ansible.module_utils.network.common.network.Config.get_config"
+        )
         self.get_config = self.mock_get_config.start()
 
         self.mock_load_config = patch(
-            'ansible.module_utils.network.common.network.Config.load_config')
+            "ansible.module_utils.network.common.network.Config.load_config"
+        )
         self.load_config = self.mock_load_config.start()
 
         self.mock_get_resource_connection_config = patch(
-            'ansible.module_utils.network.common.cfg.base.get_resource_connection')
-        self.get_resource_connection_config = self.mock_get_resource_connection_config.start()
+            "ansible.module_utils.network.common.cfg.base.get_resource_connection"
+        )
+        self.get_resource_connection_config = (
+            self.mock_get_resource_connection_config.start()
+        )
 
         self.mock_get_resource_connection_facts = patch(
-            'ansible.module_utils.network.common.facts.facts.get_resource_connection')
-        self.get_resource_connection_facts = self.mock_get_resource_connection_facts.start()
+            "ansible.module_utils.network.common.facts.facts.get_resource_connection"
+        )
+        self.get_resource_connection_facts = (
+            self.mock_get_resource_connection_facts.start()
+        )
 
         self.mock_edit_config = patch(
-            'ansible.module_utils.network.awplus.providers.providers.CliProvider.edit_config')
+            "ansible_collections.alliedtelesis.awplus.plugins.module_utils.providers.providers.CliProvider.edit_config"
+        )
         self.edit_config = self.mock_edit_config.start()
 
         self.mock_execute_show_command = patch(
-            'ansible.module_utils.network.awplus.facts.lacp.lacp.LacpFacts.get_device_data')
+            "ansible_collections.alliedtelesis.awplus.plugins.module_utils.facts.lacp.lacp.LacpFacts.get_device_data"
+        )
         self.execute_show_command = self.mock_execute_show_command.start()
 
     def tearDown(self):
@@ -50,63 +61,42 @@ class TestAwplusLacpInterfacesModule(TestAwplusModule):
         self.mock_load_config.stop()
         self.mock_execute_show_command.stop()
 
-    def load_fixtures(self, commands=None, transport='cli'):
+    def load_fixtures(self, commands=None, transport="cli"):
         def load_from_file(*args, **kwargs):
-            return load_fixture('awplus_lacp_config.cfg')
+            return load_fixture("awplus_lacp_config.cfg")
+
         self.execute_show_command.side_effect = load_from_file
 
     def test_awplus_lacp_default(self):
-        set_module_args(dict(
-            config=dict(
-                system=dict(priority=50)
-            )
-        ))
-        commands = ['lacp system-priority 50']
+        set_module_args(dict(config=dict(system=dict(priority=50))))
+        commands = ["lacp system-priority 50"]
         self.execute_module(changed=True, commands=commands)
 
     def test_awplus_lacp_default_idempotent(self):
-        set_module_args(dict(
-            config=dict(
-                system=dict(priority=32768)
-            )
-        ))
+        set_module_args(dict(config=dict(system=dict(priority=32768))))
         self.execute_module(changed=False, commands=[])
 
     def test_awplus_lacp_merged(self):
-        set_module_args(dict(
-            config=dict(
-                system=dict(priority=50)
-            ), state="merged"
-        ))
-        commands = ['lacp system-priority 50']
+        set_module_args(dict(config=dict(system=dict(priority=50)), state="merged"))
+        commands = ["lacp system-priority 50"]
         self.execute_module(changed=True, commands=commands)
 
     def test_awplus_lacp_merged_idempotent(self):
-        set_module_args(dict(
-            config=dict(
-                system=dict(priority=32768)
-            ), state="merged"
-        ))
+        set_module_args(dict(config=dict(system=dict(priority=32768)), state="merged"))
         self.execute_module(changed=False, commands=[])
 
     def test_awplus_l3_interfaces_replaced(self):
-        set_module_args(dict(
-            config=dict(
-                system=dict(priority=20)
-            ), state="replaced"
-        ))
-        commands = ['lacp system-priority 20']
+        set_module_args(dict(config=dict(system=dict(priority=20)), state="replaced"))
+        commands = ["lacp system-priority 20"]
         self.execute_module(changed=True, commands=commands)
 
     def test_awplus_l3_interfaces_replaced_idempotent(self):
-        set_module_args(dict(
-            config=dict(
-                system=dict(priority=32768)
-            ), state="replaced"
-        ))
+        set_module_args(
+            dict(config=dict(system=dict(priority=32768)), state="replaced")
+        )
         self.execute_module(changed=False, commands=[])
 
     def test_awplus_l3_interfaces_deleted(self):
         set_module_args(dict(state="deleted"))
-        commands = ['no lacp system-priority']
+        commands = ["no lacp system-priority"]
         self.execute_module(changed=True, commands=commands)
