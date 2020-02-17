5b493274aacf5677fb0c2073d0f35861401ae976
dalyIsaac <isaac.daly@outlook.com>
2020-02-17 17:02:00 +1300

    Added linkagg playbook and fixed imports for test_awplus_linkagg.py.

--- tests/unit/modules/test_awplus_linkagg.py
+++ tests/unit/modules/test_awplus_linkagg.py
@@ -16,12 +16,13 @@
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
 # Make coding more python3-ish
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 
-from units.compat.mock import patch
-from ansible.modules.network.awplus import awplus_linkagg
-from units.modules.utils import set_module_args
+from ansible_collections.alliedtelesis.awplus.tests.unit.compat.mock import patch
+from ansible_collections.alliedtelesis.awplus.plugins.modules import awplus_linkagg
+from ansible_collections.alliedtelesis.awplus.tests.unit.utils import set_module_args
 from .awplus_module import TestAwplusModule, load_fixture
 
 
@@ -33,11 +34,13 @@ class TestAwplusLinkaggModule(TestAwplusModule):
         super(TestAwplusLinkaggModule, self).setUp()
 
         self.mock_get_config = patch(
-            'ansible.modules.network.awplus.awplus_linkagg.get_config')
+            "ansible_collections.alliedtelesis.awplus.plugins.modules.awplus_linkagg.get_config"
+        )
         self.get_config = self.mock_get_config.start()
 
         self.mock_load_config = patch(
-            'ansible.modules.network.awplus.awplus_linkagg.load_config')
+            "ansible_collections.alliedtelesis.awplus.plugins.modules.awplus_linkagg.load_config"
+        )
         self.load_config = self.mock_load_config.start()
 
     def tearDown(self):
@@ -45,60 +48,68 @@ class TestAwplusLinkaggModule(TestAwplusModule):
         self.mock_get_config.stop()
         self.mock_load_config.stop()
 
-    def load_fixtures(self, commands=None, transport='cli'):
-        self.get_config.return_value = load_fixture(
-            'awplus_linkagg_config.cfg')
-        self.load_config.return_value = dict(diff=None, session='session')
+    def load_fixtures(self, commands=None, transport="cli"):
+        self.get_config.return_value = load_fixture("awplus_linkagg_config.cfg")
+        self.load_config.return_value = dict(diff=None, session="session")
 
     def test_awplus_linkagg_no_change(self):
-        set_module_args(dict(group=1, members=["port1.0.2"], mode='active'))
+        set_module_args(dict(group=1, members=["port1.0.2"], mode="active"))
         set_module_args(
-            dict(group=2, members=["port1.0.4", 'port1.0.3'], mode='passive'))
+            dict(group=2, members=["port1.0.4", "port1.0.3"], mode="passive")
+        )
         result = self.execute_module(changed=False)
-        self.assertEqual([], result['commands'])
+        self.assertEqual([], result["commands"])
 
     def test_awplus_linkagg_create(self):
         set_module_args(
-            dict(group=3, members=["port1.0.2", "port1.0.3"], mode='active'))
+            dict(group=3, members=["port1.0.2", "port1.0.3"], mode="active")
+        )
         result = self.execute_module(changed=True)
         cmds = [
-            'interface port1.0.2', 'channel-group 3 mode active',
-            'interface port1.0.3', 'channel-group 3 mode active',
+            "interface port1.0.2",
+            "channel-group 3 mode active",
+            "interface port1.0.3",
+            "channel-group 3 mode active",
         ]
-        self.assertEqual(cmds, result['commands'])
+        self.assertEqual(cmds, result["commands"])
 
     def test_awplus_linkagg_change(self):
         set_module_args(
-            dict(group=3, members=["port1.0.2", "port1.0.4"], mode="passive"))
+            dict(group=3, members=["port1.0.2", "port1.0.4"], mode="passive")
+        )
         result = self.execute_module(changed=True)
         cmds = [
-            'interface port1.0.2', 'channel-group 3 mode passive',
-            'interface port1.0.4', 'channel-group 3 mode passive'
+            "interface port1.0.2",
+            "channel-group 3 mode passive",
+            "interface port1.0.4",
+            "channel-group 3 mode passive",
         ]
-        self.assertEqual(cmds, result['commands'])
+        self.assertEqual(cmds, result["commands"])
 
     def test_awplus_linkagg_remove(self):
         set_module_args(dict(group=2, state="absent"))
         result = self.execute_module(changed=True)
         cmds = [
-            'interface port1.0.3', 'no channel-group',
-            'interface port1.0.4', 'no channel-group'
+            "interface port1.0.3",
+            "no channel-group",
+            "interface port1.0.4",
+            "no channel-group",
         ]
-        self.assertEqual(cmds, result['commands'])
+        self.assertEqual(cmds, result["commands"])
 
     def test_awplus_linkagg_add_member(self):
         set_module_args(
-            dict(group=1, members=['port1.0.2', 'port1.0.3'], mode="active"))
+            dict(group=1, members=["port1.0.2", "port1.0.3"], mode="active")
+        )
         result = self.execute_module(changed=True)
         cmds = [
-            'interface port1.0.3', 'channel-group 1 mode active',
+            "interface port1.0.3",
+            "channel-group 1 mode active",
         ]
-        self.assertEqual(cmds, result['commands'])
+        self.assertEqual(cmds, result["commands"])
 
     def test_awplus_linkagg_remove_member(self):
-        set_module_args(dict(group=2, members=['port1.0.4'], mode="passive"))
+        set_module_args(dict(group=2, members=["port1.0.4"], mode="passive"))
         result = self.execute_module(changed=True)
-        cmds = [
-            'interface port1.0.3', 'no channel-group'
-        ]
-        self.assertEqual(cmds, result['commands'])
+        cmds = ["interface port1.0.3", "no channel-group"]
+        self.assertEqual(cmds, result["commands"])
