25b5c21d82dfc17b37f1bd29e583814ab3dc3258
Darryl Alang <darrylanne.15@gmail.com>
2021-02-09 16:26:36 +1300

    NTP: Refactor as resource module - Fix parameters - Fix unit tests - Edit documentation

--- tests/unit/modules/test_awplus_ntp.py
+++ tests/unit/modules/test_awplus_ntp.py
@@ -31,73 +31,145 @@ class TestAwplusNtpModule(TestAwplusModule):
     def setUp(self):
         super(TestAwplusNtpModule, self).setUp()
 
-        self.mock_get_config = patch(
-            "ansible_collections.alliedtelesis.awplus.plugins.modules.awplus_ntp.get_config"
+        self.mock_get_resource_connection_config = patch(
+            "ansible_collections.ansible.netcommon.plugins.module_utils.network.common.cfg.base.get_resource_connection"
         )
-        self.get_config = self.mock_get_config.start()
+        self.get_resource_connection_config = self.mock_get_resource_connection_config.start()
 
-        self.mock_load_config = patch(
-            "ansible_collections.alliedtelesis.awplus.plugins.modules.awplus_ntp.load_config"
+        self.mock_get_resource_connection_facts = patch(
+            "ansible_collections.ansible.netcommon.plugins.module_utils.network.common.facts.facts.get_resource_connection"
         )
-        self.load_config = self.mock_load_config.start()
+        self.get_resource_connection_facts = self.mock_get_resource_connection_facts.start()
+
+        self.mock_edit_config = patch(
+            "ansible_collections.alliedtelesis.awplus.plugins.module_utils.providers.providers.CliProvider.edit_config"
+        )
+        self.edit_config = self.mock_edit_config.start()
+
+        self.mock_get_data = patch(
+            "ansible_collections.alliedtelesis.awplus.plugins.module_utils.network.awplus.facts.ntp.ntp.NtpFacts.get_run_conf"
+        )
+        self.get_data = self.mock_get_data.start()
 
     def tearDown(self):
         super(TestAwplusNtpModule, self).tearDown()
-        self.mock_get_config.stop()
-        self.mock_load_config.stop()
+        self.mock_get_resource_connection_config.stop()
+        self.mock_get_resource_connection_facts.stop()
+        self.mock_edit_config.stop()
+        self.mock_get_data.stop()
 
-    def load_fixtures(self, commands=None):
-        self.get_config.return_value = load_fixture("awplus_ntp_config.cfg").strip()
-        self.load_config.return_value = dict(diff=None, session="session")
+    def load_fixtures(self, commands=None, transport="cli"):
+        self.get_data.return_value = load_fixture("awplus_ntp_config.cfg")
 
     def test_awplus_ntp_idempotent(self):
         set_module_args(
             dict(
-                server="10.75.33.5",
-                source_int="192.66.44.33",
-                restrict="192.155.56.4 allow",
-                auth_key="fdasgf",
-                key_id="8900",
-                state="present",
+                config=dict(
+                    server=["10.75.33.5"],
+                    authentication=[dict(key_id=8900, key_type="md5", auth_key="fdasgf")],
+                    source_int="192.66.44.33"
+                )
             )
         )
         commands = []
         self.execute_module(changed=False, commands=commands)
 
-    def test_awplus_ntp_config(self):
+    def test_awplus_ntp_merge(self):
         set_module_args(
             dict(
-                server="10.75.33.4",
-                source_int="192.66.44.32",
-                restrict="192.155.56.3 allow",
-                auth_key="fdasgg",
-                key_id="8901",
-                state="present",
+                config=dict(
+                    server=["10.75.33.6"],
+                    authentication=[dict(key_id=1, key_type="md5", auth_key="ackrmn")],
+                )
             )
         )
-        commands = [
-            "ntp server 10.75.33.4",
-            "ntp source 192.66.44.32",
-            "ntp restrict 192.155.56.3 allow",
-            "ntp authentication-key 8901 md5 fdasgg",
-        ]
+        commands = ["ntp server 10.75.33.6", "ntp authentication-key 1 md5 ackrmn"]
         self.execute_module(changed=True, commands=commands)
 
-    def test_awplus_ntp_remove(self):
+    def test_awplus_ntp_replace(self):
         set_module_args(
             dict(
-                server="10.75.33.5",
-                source_int="192.66.44.33",
-                restrict="192.155.56.4 allow",
-                auth_key="fdasgf",
-                key_id="8900",
-                state="absent",
+                config=dict(
+                    authentication=[dict(key_id=104, key_type="md5", auth_key="jaegr")],),
+                state="replaced",
             )
         )
-        commands = [
-            "no ntp server 10.75.33.5",
-            "no ntp source",
-            "no ntp restrict 192.155.56.4 allow",
-            "no ntp authentication-key 8900 md5 fdasgf",
-        ]
+        commands = ["no ntp authentication-key 8900", "ntp authentication-key 104 md5 jaegr"]
         self.execute_module(changed=True, commands=commands)
+
+    def test_awplus_ntp_replace_server(self):
+        set_module_args(
+            dict(
+                config=dict(
+                    server=["10.75.33.7"],),
+                state="replaced",
+            )
+        )
+        commands = ["no ntp server 10.75.33.5", "ntp server 10.75.33.7"]
+        self.execute_module(changed=True, commands=commands)
+
+    def test_awplus_replace_idempotent(self):
+        set_module_args(
+            dict(
+                config=dict(authentication=[dict(key_id=8900, key_type="md5", auth_key="fdasgf")],),
+                state="replaced",
+            )
+        )
+        commands = []
+        self.execute_module(changed=False, commands=commands)
+
+    def test_awplus_ntp_overridden(self):
+        set_module_args(
+            dict(
+                config=dict(
+                    authentication=[dict(key_id=104, key_type="md5", auth_key="jaegr")],),
+                state="overridden",
+            )
+        )
+        commands = ["no ntp server 10.75.33.5", "no ntp source", "no ntp authentication-key 8900", "ntp authentication-key 104 md5 jaegr"]
+        self.execute_module(changed=True, commands=commands)
+
+    def test_awplus_overridden_idempotent(self):
+        set_module_args(
+            dict(
+                config=dict(
+                    server=["10.75.33.5"],
+                    authentication=[dict(key_id=8900, key_type="md5", auth_key="fdasgf")],
+                    source_int="192.66.44.33"
+                ),
+                state="overridden",
+            )
+        )
+        commands = []
+        self.execute_module(changed=False, commands=commands)
+
+    def test_awplus_ntp_deleted(self):
+        set_module_args(
+            dict(
+                config=dict(
+                    authentication=[dict(key_id=8900, key_type="md5", auth_key="fdasgf")],),
+                state="deleted",
+            )
+        )
+        commands = ["no ntp authentication-key 8900"]
+        self.execute_module(changed=True, commands=commands)
+
+    def test_awplus_deleted_all(self):
+        set_module_args(
+            dict(
+                state="deleted",
+            )
+        )
+        commands = ["no ntp server 10.75.33.5", "no ntp source", "no ntp authentication-key 8900"]
+        self.execute_module(changed=True, commands=commands)
+
+    def test_awplus_ntp_deleted_none(self):
+        set_module_args(
+            dict(
+                config=dict(
+                    authentication=[dict(key_id=104, key_type="md5", auth_key="fdasgf")],),
+                state="deleted",
+            )
+        )
+        commands = []
+        self.execute_module(changed=False, commands=commands)
